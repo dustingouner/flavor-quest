3a206ad9beeb734a550b3b51b3fccc8c1f01c9b5
Dustin Gouner <dustingouner20@gmail.com>
2023-06-06 22:03:38 -0600

    refac: passed in newMeal as prop which will allow the user to see a new randomized meal upon button click without having to go back to the home page

--- src/Components/MealDetail/MealDetail.js
+++ src/Components/MealDetail/MealDetail.js
@@ -3,25 +3,21 @@ import './MealDetail.css'
 import { Link } from 'react-router-dom'
 import fetchData from "../../ApiCalls";
 
-function MealDetail({ randomMeal }) {  //pass in randomMeal function to be called onClick of new meal button
+function MealDetail({ randomMeal, newMeal }) {  //pass in randomMeal function to be called onClick of new meal button
   console.log(randomMeal[0])
   const meal = randomMeal[0]
-  let rando
-  const getRandomMeal= () => {
-    fetchData() 
-    .then(data => rando = data)
-    
-    
-  }
+  
+  
 
   return (
     <div>
       <img className="meal-image" src={meal.strMealThumb}/>
       <h1 className="meal-name">{meal.strMeal}</h1>
       <p className="meal-instructions">{meal.strInstructions}</p>
+
       <button>Shopping List</button>
       <Link to='/randomMeal'>
-        <button onClick={(event) => getRandomMeal(event)}>New Meal</button>
+        <button onClick={(event) => newMeal(event)}>New Meal</button>
         </Link>
     </div>
   )
