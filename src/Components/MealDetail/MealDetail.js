874f8941c6836f94d58b7815125ff69de44b0753
Dustin Gouner <dustingouner20@gmail.com>
2023-06-06 23:34:24 -0600

    feat: add cooking instruction video to meal detail page

--- src/Components/MealDetail/MealDetail.js
+++ src/Components/MealDetail/MealDetail.js
@@ -6,20 +6,38 @@ import fetchData from "../../ApiCalls";
 function MealDetail({ randomMeal, newMeal }) {  //pass in randomMeal function to be called onClick of new meal button
   console.log(randomMeal[0])
   const meal = randomMeal[0]
+
+  const videoUrl = meal.strYoutube;
+  const videoId = videoUrl.split('v=')[1];
+
+  // Construct the embed URL
+  const embedUrl = `https://www.youtube.com/embed/${videoId}`
   
   
 
   return (
     <div>
-      <img className="meal-image" src={meal.strMealThumb}/>
-      <h1 className="meal-name">{meal.strMeal}</h1>
+      <img className="meal-image" src={meal.strMealThumb} alt="Meal Thumbnail" />
+      <h1 className="meal-name">{`${meal.strMeal} | ${meal.strArea} | ${meal.strCategory}`}</h1>
       <p className="meal-instructions">{meal.strInstructions}</p>
 
       <button>Shopping List</button>
-      <Link to='/randomMeal'>
-        <button onClick={(event) => newMeal(event)}>New Meal</button>
-        </Link>
+      <Link to="/randomMeal">
+        <button onClick={newMeal}>New Meal</button>
+      </Link>
+
+    <div id="player">
+      <iframe
+        title="Meal Video"
+        width="560" // Set the desired width
+        height="315" // Set the desired height
+        border="0"
+        src={embedUrl}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
+  </div>
   )
 }
 
