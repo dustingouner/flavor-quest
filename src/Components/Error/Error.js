aea5481e7bc72eecd650d56c1231719a8006a5bd
Dustin Gouner <dustingouner20@gmail.com>
2023-06-10 12:54:23 -0600

    set up error handling if user goes to wrong url path

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,17 +1,17 @@
-import React from "react";
-import './Error.css'
+import React from 'react';
+import './Error.css';
 
+const Error = ({ location, errorMessage }) => {
+  const displayMessage = errorMessage
+    ? errorMessage : `The URL path ${location.pathname} you used was incorrect. Please try again.`;
 
-const Error = ({errorMessage}) => {
   return (
     <div className="error-container">
       <div className="error-message-container">
-        <p className="error-message">{errorMessage}</p>
+        <p className="error-message">{displayMessage}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default Error
+export default Error;
